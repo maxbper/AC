28b767f0392582c342fc4b32b6121da705752e23
Ema Martins <150731688+ema-12-martins@users.noreply.github.com>
2024-10-05 13:55:37 +0100

    passar o nome do csv como argumento

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 import os
-import queries as q  # Assuming the functions are in 'queries'
+import sys
+import queries as q 
 
-# Get the current directory and the path to the dataset
-current_dir = os.getcwd()
-datasets_dir = os.path.join(current_dir, 'datasets')
+if len(sys.argv) > 1:
 
-# Path to the 'awards_players.csv' file
-awards_players_path = os.path.join(datasets_dir, 'awards_players.csv')
+    # Get the current directory and the path to the dataset
+    current_dir = os.getcwd()
+    datasets_dir = os.path.join(current_dir, 'datasets')
 
-try:
-    # Read the dataset
-    file = q.read_dataset(awards_players_path) 
+    # Path to the 'awards_players.csv' file
+    awards_players_path = os.path.join(datasets_dir, sys.argv[1])
 
-    # Calculate and print the mean, median, and mode
-    q.mean_for_each_column(file)
-    q.median_for_each_column(file)
-    q.mode_for_each_column(file)
-    
-    #Box-plot for all numeric columns
-    q.box_plot_for_each_column(file)
-    
-    #Bar-chart for all numeric columns
-    q.bar_chart_for_each_column(file)
+    try:
+        # Read the dataset
+        file = q.read_dataset(awards_players_path) 
 
-except FileNotFoundError:
-    print("File not found.")
-except Exception as e:
-    print(f"Error: {e}")
+        # Calculate and print the mean, median, and mode
+        q.mean_for_each_column(file)
+        q.median_for_each_column(file)
+        q.mode_for_each_column(file)
+
+        #Box-plot for all numeric columns
+        q.box_plot_for_each_column(file)
+
+        #Bar-chart for all numeric columns
+        q.bar_chart_for_each_column(file)
+
+    except FileNotFoundError:
+        print("File not found.")
+    except Exception as e:
+        print(f"Error: {e}")
+else:
+    print("No argument passed.")
